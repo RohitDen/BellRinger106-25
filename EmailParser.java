2209bdbe549a99bcd7f504c9c51ccbe956062a1b
RohitDen <157422362+RohitDen@users.noreply.github.com>
2025-10-06 13:55:08 -0700

    Update EmailParser.java

--- EmailParser.java
+++ EmailParser.java
@@ -1,1 +1,16 @@
+import java.util.Scanner;
 
+public class EmailParser {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type your email here:");
+        String Email = sc.nextLine();
+        
+        System.out.println("Your first name: ");
+        String FirstName = sc.nextLine();
+        
+        System.out.println("Your last name: ");
+        String LastName = sc.nextline();
+        
+    }
+}
